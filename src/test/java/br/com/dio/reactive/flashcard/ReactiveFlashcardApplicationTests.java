81442c4efda3f39d1250825b4ddaad6b577ad0d2
Janderson Cassan <jande.max@gmail.com>
2024-11-09 18:52:32 -0300

    initial commit project

---
+++ src/test/java/br/com/dio/reactive/flashcard/ReactiveFlashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.reactive.flashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
